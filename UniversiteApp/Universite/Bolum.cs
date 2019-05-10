20c61498be6a01a671fddbd6f846585d0b533799
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-11 01:27:14 +0300

    Bolum sınıfına temel değişkenler eklendi ve method tanımlamaları kısmen (veya ismen) yapıldı

--- UniversiteApp/Universite/Bolum.cs
+++ UniversiteApp/Universite/Bolum.cs
@@ -8,5 +8,39 @@ namespace UniversiteApp
 {
     class Bolum
     {
+        public String BolumAdi { get; set; }
+        public List<Ders> dersler = new List<Ders>();
+        public Fakulte fakulte;
+        public List<OgretimElemani> ogretimElemanlari = new List<OgretimElemani>();
+
+        public void OgrenciKayit()
+        {
+            //TODO Ogrenci sınıfındaki kayıt methoduyla ve direk sınıfın kendisi ile ilişkilendir
+        }
+
+        public void OgrenciSil()
+        {
+            //TODO Ogrenci sınıfıyla ilişkilendir
+        }
+
+        public void OgretimElemaniAta()
+        {
+            //TODO OgretimElemani sınıfıyla ilişkilendir
+        }
+
+        public void OgretimElemaniSil()
+        {
+            //TODO OgretimElemani sınfıyla ilişkilendir
+        }
+
+        public void DersAc()
+        {
+            //TODO Ders sınıfı ile ilişkilendir
+        }
+
+        public void DersKapa()
+        {
+            //TODO Ders sınıfı ile ilişkilendir veya sadece List üzeründen müdahale et
+        }
     }
 }
