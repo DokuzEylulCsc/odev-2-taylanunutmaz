6d53885f873b5e3f7436ff7f3ee97dada5cef62e
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-11 00:53:44 +0300

    Öğrenci sınıfı değişkenleri tanımlandı ve gerekli temel fonksiyonlar eklendi

--- UniversiteApp/Kisiler/Ogrenci.cs
+++ UniversiteApp/Kisiler/Ogrenci.cs
@@ -8,5 +8,70 @@ namespace UniversiteApp
 {
     class Ogrenci
     {
+        public String Ad { get; set; }
+        public String Soyad { get; set; }
+        public int Numara { get; set; }
+        List<Ders> dersler = new List<Ders>(10);
+        public Bolum Bolum { get; set; }
+
+        public Ogrenci()
+        {
+            OgrenciKayit();
+        }
+
+        private void OgrenciKayit(int retryNumber = 0)
+        {
+            Console.WriteLine("Yeni Öğrenci Kaydı Oluşturuluyor.");
+            Console.WriteLine("Öğrenci Adı:");
+            const int maxRetryNumber = 3; //maxRetryNumber için bkz(https://stackoverflow.com/a/9272075)
+            try
+            {
+                Ad = Console.ReadLine().ToString();
+            }
+            catch (Exception)
+            {
+                if (retryNumber < maxRetryNumber)
+                {
+                    Console.WriteLine("Bir şeyler yanlış gitti. Hata kodu 0xMicrosoft. " +
+                        "Lütfen kontrol edip tekrar deneyin.");
+                    OgrenciKayit(retryNumber++);
+                }
+                else
+                {
+                    Console.WriteLine("Çok sayıda hatalı giriş yapıldıği için " +
+                        "kayıt işlemi sonlandırılıyor!");
+                    throw;
+                }
+            }
+
+            Console.WriteLine("Öğrenci Soyadı:");
+            try
+            {
+                Soyad = Console.ReadLine().ToString();
+            }
+            catch (Exception)
+            {
+                if (retryNumber < maxRetryNumber)
+                {
+                    Console.WriteLine("Bir şeyler yanlış gitti. Hata kodu 0xMicrosoft. " +
+                        "Lütfen kontrol edip tekrar deneyin.");
+                    OgrenciKayit(retryNumber++);
+                }
+                else
+                {
+                    Console.WriteLine("Çok sayıda hatalı giriş yapıldıği için " +
+                        "kayıt işlemi sonlandırılıyor!");
+                    throw;
+                }
+            }
+
+            //TODO Kayıt sırasında bölüm ekleme yap
+
+            Random rd = new Random();
+            Numara = DateTime.Now.Year * 1000000 +  rd.Next(100000, 999999);
+
+            Console.WriteLine($"{Ad} {Soyad} adlı öğrenci {Numara} numarasıyla başarıyla kaydedildi.");
+            
+        }
     }
 }
--- UniversiteApp/Universite/Universite.cs
+++ UniversiteApp/Universite/Universite.cs
@@ -8,5 +8,6 @@ namespace UniversiteApp
 {
     class Universite
     {
+
     }
 }
