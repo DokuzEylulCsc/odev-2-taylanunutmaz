73d88a62fbf47f5fd109581e71b09886b29322db
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-11 01:18:54 +0300

    Fakulte sınıfına temel değişkenler ve methodlar eklendi

--- UniversiteApp/Universite/Fakulte.cs
+++ UniversiteApp/Universite/Fakulte.cs
@@ -8,5 +8,13 @@ namespace UniversiteApp
 {
     class Fakulte
     {
+        public String FakulteAdi { get; set; }
+        public Universite universite { get; set; }
+        public List<Bolum> bolumler = new List<Bolum>();
+
+        public Fakulte(string fakulteAdi)
+        {
+            FakulteAdi = fakulteAdi;
+        }
     }
 }
