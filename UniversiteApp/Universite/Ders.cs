763d8a8276b90424fb89eaff22707896086bffac
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-11 01:34:35 +0300

    Ders sınıfına temel değişkenler eklendi ve methodlar kısmen tanımlandı

--- UniversiteApp/Universite/Ders.cs
+++ UniversiteApp/Universite/Ders.cs
@@ -8,5 +8,43 @@ namespace UniversiteApp
 {
     class Ders
     {
+        public String DersAdi { get; set; }
+        public int Yil { get; set; }
+        public int Donem { get; set; }
+        public List<Sube> subeler = new List<Sube>();
+        public Bolum Bolum { get; set; }
+
+        public Ders(string dersAdi, int yil, int donem, Bolum bolum)
+        {
+            DersAdi = dersAdi;
+            Yil = yil;
+            Donem = donem;
+            this.Bolum = bolum;
+        }
+
+        public void OgretimElemaniAta()
+        {
+            //TODO
+        }
+
+        public void OgretimElemaniSil()
+        {
+            //TODO
+        }
+
+        public void OgrenciEkle()
+        {
+            //TODO
+        }
+
+        public void OgrenciSil()
+        {
+            //TODO
+        }
+
+        public void DosyayaKaydet()
+        {
+            //TODO Ek olarak DosyadanOkuma methodunu da değerlendir
+        }
     }
 }
