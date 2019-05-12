0b082871e13ed94a434c97666c162548916f64ee
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-13 00:47:15 +0300

    Bolum sınıfındaki Ogrenci ve Ogretim Elemanı için kayıt ve silme işlemlerini yapan methodlar yeni kontrol ifadeleri (if) ile düzenlendi

--- UniversiteApp/Universite/Bolum.cs
+++ UniversiteApp/Universite/Bolum.cs
@@ -25,36 +25,71 @@ namespace UniversiteApp
         {
             //Bu method öğrencinin bölüm değiştirmesi yada bölümü silinen öğrencilerin 
             //yeniden bir bölüme atanması için kullanılacaktır.
-            ogrenci.Bolum = this;
-            ogrenciler.Add(ogrenci);
-            Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
-                $"{BolumAdi} isimli bölüme başarıyla kaydedildi!");
+            if (!ogrenciler.Contains(ogrenci) && ogrenci.Bolum == null)
+            {
+                ogrenci.Bolum = this;
+                ogrenciler.Add(ogrenci);
+                Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
+                    $"{BolumAdi} isimli bölüme başarıyla kaydedildi!");
+            }
+            else
+            {
+                Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
+                    $"zaten {ogrenci.Bolum.BolumAdi} isimli bölüme kayıtlı olduğundan " +
+                    $"{BolumAdi} isimli bölüme kaydedilemedi!");
+            }
         }
 
         public void OgrenciSil(Ogrenci ogrenci)
         {
-            ogrenci.Bolum = null;
-            ogrenciler.Remove(ogrenci);
-            Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
-                $"{BolumAdi} isimli bölümden başarıyla silindi.");
+            if (ogrenciler.Contains(ogrenci))
+            {
+                ogrenci.Bolum = null;
+                ogrenciler.Remove(ogrenci);
+                Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
+                    $"{BolumAdi} isimli bölümden başarıyla silindi.");
+            }
+            else
+            {
+                Console.WriteLine($"{ogrenci.Ad} {ogrenci.Soyad} isimli öğrenci " +
+                    $"zaten {BolumAdi} isimli bölüme kayatılı değil!");
+            }
         }
 
         public void OgretimElemaniAta(OgretimElemani ogretimElemani)
         {
             //Bölümü değiştirilen veya daha önceden bölümü silinen öğretim elemanına 
             //bölüm atamanak için kullanılacak method
-            ogretimElemani.Bolum = this;
-            ogretimElemanlari.Add(ogretimElemani);
-            Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
-                $"{BolumAdi} isimli bölüme atandı.");
+            if (!ogretimElemanlari.Contains(ogretimElemani) && ogretimElemani.Bolum == null)
+            {
+                ogretimElemani.Bolum = this;
+                ogretimElemanlari.Add(ogretimElemani);
+                Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
+                    $"{BolumAdi} isimli bölüme atandı.");
+
+            }
+            else
+            {
+                Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
+                    $"zaten {ogretimElemani.Bolum.BolumAdi} isimli bölüme kayıtlı olduğundan " +
+                    $"{BolumAdi} isimli bölüme kaydedilemedi!");
+            }
         }
 
         public void OgretimElemaniSil(OgretimElemani ogretimElemani)
         {
-            ogretimElemani.Bolum = null;
-            ogretimElemanlari.Remove(ogretimElemani);
-            Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
-                $"{BolumAdi} isilmi bölümden başarıyla kaldırıldı.");
+            if (ogretimElemanlari.Contains(ogretimElemani))
+            {
+                ogretimElemani.Bolum = null;
+                ogretimElemanlari.Remove(ogretimElemani);
+                Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
+                    $"{BolumAdi} isilmi bölümden başarıyla kaldırıldı.");
+            }
+            else
+            {
+                Console.WriteLine($"{ogretimElemani.Ad} {ogretimElemani.Soyad} isimli öğretim elemanı " +
+                    $"zaten {BolumAdi} isimli bölüme kayıtlı değil!");
+            }
         }
 
         public void DersAc()
